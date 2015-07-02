2f38942cc1e8fdd1a08963be39966fd1fffbe5db
Gaz <gareth.street@squelchdesign.com>
2015-07-02 16:38:00 +0100

    initial commit

---
+++ greet_user.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+Plugin Name: Greet User
+*/ 
+	function greet_user() {
+		$username = $_GET['user'];
+		if( $username ){
+			echo "Hi ". $username ."!";
+		} else {
+			echo "Why not tell me your name";
+		}
+	}
+	
+	function greet_user_shortcode( $atts, $content, $tag ){
+		return greet_user();
+	}
+	add_shortcode('greet_user', 'greet_user_shortcode'); 
+?>
